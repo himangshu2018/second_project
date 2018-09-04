7d0771dfb8dde814b15ae246b334bbe1c2a66085
unknown <himangahu.zoadder@gmail.com>
2018-09-04 12:56:18 -0400

    add another project

---
+++ mavenProject/src/test/java/mavenProject/BaseTest.java
@@ -0,0 +1,19 @@
+package mavenProject;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+
+public class BaseTest {
+	@BeforeTest
+	public void configFile()
+	{
+		
+	}
+	
+	@BeforeMethod
+	public void  start()
+	{
+		
+	}
+
+}
